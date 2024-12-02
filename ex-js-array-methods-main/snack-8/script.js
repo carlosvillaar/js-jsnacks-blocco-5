7831b6203d00e7dcc9e57505d458d156ebcb3ebb
carlosvillaar <carlosvillar004@gmail.com>
2024-12-02 14:54:54 +0100

    feat: try to solve 8th exercice

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -3,21 +3,36 @@ const students = [
     id: 1,
     name: 'Marco Lanci',
     age: 32,
-    class: '3C'
+    classe: '3C'
   },
   {
     id: 2,
     name: 'Mario Banfi',
     age: 34,
-    class: '4A'
+    classe: '4A'
   },
   {
     id: 3,
     name: 'Luigi Banzi',
     age: 33,
-    class: '5B'
+    classe: '5B'
   },
 ];
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+/*const student = students.filter((student) => student.class === '3C');
+
+console.log(student);
+*/
+
+const student = students.map((student) => {
+  const myStudent = 'Marco Lanci';
+  if (student.name === myStudent){
+    return student.classe;
+  }
+});
+
+console.log(student);
+
