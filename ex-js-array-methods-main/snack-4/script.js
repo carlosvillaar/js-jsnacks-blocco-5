3e3408fbd01e1904c151935f3aff694abf8e4c8a
carlosvillaar <carlosvillar004@gmail.com>
2024-12-02 15:49:03 +0100

    fix: fix 4th exercice

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -8,4 +8,5 @@ const posts = [
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
 
-const authors = posts.map(({author}) => console.log({author}));
+const authors = posts.map((post) => post.author);
+console.log(authors);
