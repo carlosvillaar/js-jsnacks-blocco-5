4eeefc1cbbf67f7bb7be040d4297733718b4cd5c
carlosvillaar <carlosvillar004@gmail.com>
2024-12-02 14:03:48 +0100

    feat: add first soluction

---
+++ ex-js-array-methods-main/snack-1/script.js
@@ -0,0 +1,9 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+for (let name of names){
+  console.log(name);
+  
+}
