684c2131910a8466e183f2ef4a2674fdb4921191
carlosvillaar <carlosvillar004@gmail.com>
2024-12-02 14:36:43 +0100

    feat: add 7th solution

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -21,3 +21,11 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const studentTwo = students.filter((student) => {
+if (student.id === 2){
+  return student;
+}
+});
+console.log(studentTwo);
+
